ff091fa96b01cfb2ca953c809a7e3d982dda1d67
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-25 14:43:33 -0300

    refactor: enable Character to attack SimpleFighter
    - Replace type of `attack` and `special` methods to `SimpleFighter`

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -65,11 +65,11 @@ export default class Character implements Fighter {
     };
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this.strength);
   }
 
-  special(enemy: Fighter): void {
+  special(enemy: SimpleFighter): void {
     console.log(`
     No special move for ${this._archetype} discovered yet!
     Keep training to reach ${enemy.strength}!
