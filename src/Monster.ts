87ff63adda8d5c13897cc2b654c81654020921c7
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-25 14:41:28 -0300

    feat: create Monster class

---
+++ src/Monster.ts
@@ -0,0 +1,32 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this.lifePoints;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+    if (this.lifePoints < 1) this._lifePoints = -1;
+    return this.lifePoints;
+  }
+}
